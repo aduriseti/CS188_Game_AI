a9c2954c66f58be945da9bb966133ad36d4531f9
Mitchell Binning <MitchellBinning@gmail.com>
2016-04-18 13:46:36 -0700

    Basic Mouse AI Skeleton

---
+++ Game/Scripts/AI/Behaviors/OnMealCompleteBehavior.lua
@@ -0,0 +1,7 @@
+-- Custom Signal Behavior
+
+OnMealComplete = function(self, entity, distance)
+	-- Called when OnMealComplete is received and this behavior is active
+
+	-- NOT SURE WTF TO DO IN THIS....
+end
